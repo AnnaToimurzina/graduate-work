ff4681573dc0ad1928e61ff6ebb3f513b778ca56
ANNA TOIMURZINA <annatoimurzina@MacBook-Pro-Anna.local>
2023-11-28 15:17:57 +0500

    дописаны settings

--- config/settings.py
+++ config/settings.py
@@ -133,3 +133,12 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL='users.User'
+
+# Настройки JWT-токенов
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+# Замена на права доступа для всех, права на авторизацию указываются отдельно
+    'DEFAULT_PERMISSION_CLASSES': [
+   'rest_framework.permissions.AllowAny',]}
