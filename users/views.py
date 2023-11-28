a505fa17fa0751e0f5d4058d0cab123fd01cf268
ANNA TOIMURZINA <annatoimurzina@MacBook-Pro-Anna.local>
2023-11-28 15:17:45 +0500

    дописаны view users

--- users/views.py
+++ users/views.py
@@ -1,11 +1,29 @@
-from rest_framework import viewsets
+from rest_framework import generics, viewsets
+
 from users.models import User
-from .serializers import UserSerializer
+from users.serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
     serializer_class = UserSerializer
+    queryset = User.objects.all()
+class UserCreateView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+class UserListView(generics.ListCreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+class UserRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+class UserDestroyAPIView(generics.DestroyAPIView):
+    queryset = User.objects.all()
+
 
 
 
