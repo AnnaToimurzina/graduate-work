5934b22ea158c529623589ada77956ec6744de87
ANNA TOIMURZINA <annatoimurzina@MacBook-Pro-Anna.local>
2023-12-01 09:42:46 +0500

    дописан tasks

---
+++ tasks/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from users.models import User
+
+class Task(models.Model):
+    task_name = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    parent_task = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+    executor = models.ForeignKey(User, on_delete=models.CASCADE)
+    deadline = models.DateField()
+    STATUS_CHOICES = [
+        ('pending', 'Ожидает'),
+        ('processing', 'В процессе'),
+        ('completed', 'Завершено'),
+        ('failed', 'Не удалось')]
+
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+
+
+
+    def __str__(self):
+        return f"{self.task_name} {self.executor} {self.deadline}"
+
+    class Meta:
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+
