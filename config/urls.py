ed629c4bf66a9372098c705350963775545493b7
ANNA TOIMURZINA <annatoimurzina@MacBook-Pro-Anna.local>
2023-12-05 09:59:28 +0500

    добавлен swagger

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API",
+        default_version='v1',
+        description="API description",
+        terms_of_service="https://www.graduate_work.com/terms/",
+        contact=openapi.Contact(email="2402as@gmail.com"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,))
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tasks.urls', namespace='tasks')),
     path('users/', include('users.urls', namespace='users')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
